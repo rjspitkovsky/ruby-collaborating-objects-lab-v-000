da7210ae61ebdbbed883eb741d816c345eca5a51
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-02-07 01:26:39 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,6 +34,7 @@ class Song
 
   def artist_name=(artist_name)#takes in the string, runs the find_or_create code then assigns an Artist object
     self.artist = Artist.find_or_create_by_name(artist_name)
+    self.artist.add_song(self.name)
 
 
 
